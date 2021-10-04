a31602564321945e6d1d25330394350721042a3e
Thállen Galvão <thallen.galvao@AvilaSolucoes.intranet>
2021-10-04 11:04:48 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+   const state = useSelector((state) => state)
+   const dispatch = useDispatch()
+   console.log(state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Total: {state}</h1>
+      <button onClick={() => dispatch({type: 'INCREMENTAR'})}>Incrementar</button>
     </div>
   );
 }
